7d8b26f98f29f1ba551cd722eaa0cea4a8bda86d
special.fy <special.fy@alibaba-inc.com>
2021-05-26 18:01:34 +0800

    Add

---
+++ examples/host_regex_rewrite/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
+	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
+)
+
+func main() {
+	proxywasm.SetNewRootContext(newRootContext)
+}
+
+type rootContext struct {
+	// You'd better embed the default root context
+	// so that you don't need to reimplement all the methods by yourself.
+	proxywasm.DefaultRootContext
+}
+
+func newRootContext(uint32) proxywasm.RootContext { return &rootContext{} }
+
+// Override DefaultRootContext.
+func (*rootContext) NewHttpContext(contextID uint32) proxywasm.HttpContext {
+	return &httpHeaders{contextID: contextID}
+}
+
+type httpHeaders struct {
+	// You'd better embed the default root context
+	// so that you don't need to reimplement all the methods by yourself.
+	proxywasm.DefaultHttpContext
+	contextID uint32
+}
+
+// Override DefaultHttpContext.
+func (ctx *httpHeaders) OnHttpRequestHeaders(numHeaders int, endOfStream bool) types.Action {
+	err := proxywasm.SetHttpRequestHeader("test", "best")
+	if err != nil {
+		proxywasm.LogCritical("failed to set request header: test")
+	}
+
+	hs, err := proxywasm.GetHttpRequestHeaders()
+	if err != nil {
+		proxywasm.LogCriticalf("failed to get request headers: %v", err)
+	}
+
+	for _, h := range hs {
+		proxywasm.LogInfof("request header --> %s: %s", h[0], h[1])
+	}
+	return types.ActionContinue
+}
+
+// Override DefaultHttpContext.
+func (ctx *httpHeaders) OnHttpResponseHeaders(numHeaders int, endOfStream bool) types.Action {
+	hs, err := proxywasm.GetHttpResponseHeaders()
+	if err != nil {
+		proxywasm.LogCriticalf("failed to get response headers: %v", err)
+	}
+
+	for _, h := range hs {
+		proxywasm.LogInfof("response header <-- %s: %s", h[0], h[1])
+	}
+	return types.ActionContinue
+}
+
+// Override DefaultHttpContext.
+func (ctx *httpHeaders) OnHttpStreamDone() {
+	proxywasm.LogInfof("%d finished", ctx.contextID)
+}
